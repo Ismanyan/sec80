12fe3d3910857c2b0bb3de2944b8b3d08907da06
Bilkis Ismail <bilkisismail007@gmail.com>
2019-07-18 16:26:57 +0700

    Stable v.1

---
+++ index.php
@@ -0,0 +1,392 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <link rel="shortcut icon" href="img/favicon.png">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="author" content="Ismanyan">
+  <meta name="keywords" content="software engineering club 80">
+  <meta name="description" content="Belajar pemrograman bersama kami di software engineering club 80">
+
+  <title>SEC 80</title>
+
+  <!-- Font Awesome Icons -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+
+  <!-- Plugin CSS -->
+  <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+  <!-- Theme CSS - Includes Bootstrap -->
+  <link href="css/creative.css" rel="stylesheet">
+  <script src="vendor/sweetalert/sweetalert.min.js" charset="utf-8"></script>
+  <link rel="stylesheet" href="vendor/sweetalert/sweetalert.css">
+  <link rel="stylesheet" href="css/testimonial.css">
+  <link rel="stylesheet" href="css/agency.css">
+  <link rel="stylesheet" href="css/footer.css">
+</head>
+
+<body id="page-top">
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">SEC 8O</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav mr-auto my-2 my-lg-0">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#portfolio">Gallery</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Masthead -->
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100">
+        <div class="col-lg-5 align-self-end">
+          <h1 class="text-uppercase text-white font-weight-bold">Belajar Pemrograman Bersama Software Engineering Club</h1>
+        </div>
+        <div class="col-lg-8 align-self-baseline">
+          <a class="btn btn-primary btn-xl js-scroll-trigger find-out-more" href="#about">Find Out More</a>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- About Section -->
+  <section class="page-section bg-primary" id="about">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="text-white mt-0">Software Engineering Club</h2>
+          <hr class="divider light my-4">
+          <p class="text-white-50 mb-4">
+            Software Engineering Club adalah sebuah perkumpulan siswa SMKN 4 Kota Tangerang yang memiliki ketertarikan
+            pada bidang komputer khususnya Pemrograman dan tujuan utama kita adalah belajar bersama dari nol sampai
+            jadi seorang juara
+          </p>
+          <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Get Started!</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Services Section -->
+  <section class="page-section" id="services">
+    <div class="container">
+      <h2 class="text-center mt-0">Apa yang kamu dapat</h2>
+      <hr class="divider my-4">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <img src="img/icon/schedule.png" class="mb-4" alt="Schedule" width="80">
+            <h3 class="h4 mb-2">Waktu Belajar</h3>
+            <p class="text-muted mb-0">2 Jam belajar bersama kami di hari sabtu (07.00 - 10.00 WIB)</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <img src="img/icon/targeting.png" class="mb-4" alt="Targeting" width="80">
+            <h3 class="h4 mb-2">Materi Terbaik</h3>
+            <p class="text-muted mb-0">Materi dan pembelajaran terkait Pemrograman yang terkini</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <img src="img/icon/content.png" class="mb-4" alt="Content" width="80">
+            <h3 class="h4 mb-2">Belajar Asik</h3>
+            <p class="text-muted mb-0">Belajar yang gak cuman teori doang tapi langsung praktek</p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <img src="img/icon/piggy-bank.png" class="mb-4" alt="Gratis" width="80">
+            <h3 class="h4 mb-2">Belajar Gratis</h3>
+            <p class="text-muted mb-0">Semuanya pembelajaran di dapatkan secara GRATIS</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Portfolio Section -->
+  <section id="portfolio">
+    <div class="container-fluid p-0">
+      <div class="row no-gutters">
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/full_1.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/min_1.jpg" alt="thumbnails">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Kegiatan
+              </div>
+              <div class="project-name">
+                Software Engineering Club
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/full_2.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/min_2.jpg" alt="thumbnails">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Kegiatan
+              </div>
+              <div class="project-name">
+                Software Engineering Club
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/full_3.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/min_3.jpg" alt="thumbnails">
+            <div class="portfolio-box-caption p-3">
+              <div class="project-category text-white-50">
+                Kegiatan
+              </div>
+              <div class="project-name">
+                Software Engineering Club
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Call to Action Section -->
+  <section class="page-section text-white daftar">
+    <div class="container text-center">
+      <h2 class="mb-4">Ayo gabung sekarang bersama kami</h2>
+      <hr class="divider light my-4">
+      <a class="btn btn-light btn-xl" href="#" data-toggle="modal" data-target="#daftar">Daftar Sekarang</a>
+    </div>
+  </section>
+
+  <!-- Testimonial -->
+  <!-- <section class="testimonial-bg shadow-sm" id="testimonial">
+    <h2 class="text-white">Apa Kata Mereka</h2>
+    <hr class="divider light my-4">
+    <div class="card text-center shadow-lg p-1">
+      <div id="myCarousel" class="carousel slide" data-ride="carousel"> -->
+        <!-- Wrapper for carousel items -->
+        <!-- <div class="carousel-inner">
+          <div class="item carousel-item active">
+            <div class="img-box"><img src="img/testimoni/1.jpg" alt="bilkis"></div>
+            <p class="testimonial">Magang di Positive Republik itu menurut saya suatu pengalaman yang sangat berharga karena hampir semua ilmu yang di berikan disana dapat di terapkan di dunia kerja</p>
+            <p class="overview"><b>Bilkis Ismail</b>, Alumni Positive Republik Academy</p>
+          </div>
+          <div class="item carousel-item">
+            <div class="img-box"><img src="img/testimoni/2.jpeg" alt="eben"></div>
+            <p class="testimonial">Magang di Positive Republik menyenangkan. Saya juga mendapat pengalaman belajar tentang internet marketing yang akan sangat berguna jika sudah lulus nanti</p>
+            <p class="overview"><b>Eben Ezer</b>, Alumni Positive Republik Academy</p>
+          </div>
+          <div class="item carousel-item">
+            <div class="img-box"><img src="img/testimoni/3.jpg" alt="rafli"></div>
+            <p class="testimonial">Magang di positif Republik itu seru karena pembimbing nya asik-asik, dan saya juga mendapat ilmu tentang Internet marketing yg bisa saya gunakan untuk berbisnis nanti</p>
+            <p class="overview"><b>Rafli Zacky</b>, Alumni Positive Republik Academy</p>
+          </div>
+        </div> -->
+        <!-- Carousel controls -->
+        <!-- <a class="carousel-control left carousel-control-prev" href="#myCarousel" data-slide="prev">
+          <i class="fa fa-angle-left"></i>
+        </a>
+        <a class="carousel-control right carousel-control-next" href="#myCarousel" data-slide="next">
+          <i class="fa fa-angle-right"></i>
+        </a>
+      </div>
+    </div>
+  </section> -->
+
+  <!-- Contact Section -->
+  <section class="page-section" id="contact">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="mt-0">Kontak Kami</h2>
+          <hr class="divider my-4 mb-5">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+          <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+          <a class="d-block" href="tel:[089630080545]">+62 896-3008-0545</a>
+        </div>
+        <div class="col-lg-4 mr-auto text-center">
+          <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+          <!-- Make sure to change the email address in anchor text AND the link below! -->
+          <a class="d-block" href="mailto:bilkisismail07@gmail.com">bilkisismail07@gmail.com</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Contact -->
+  <!-- <section class="page-section" id="saran">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h2 class="section-heading text-uppercase">HUBUNGI KAMI</h2>
+          <hr class="divider light my-4">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <form id="contactForm" name="sentMessage" novalidate="novalidate">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <input class="form-control" id="name" type="text" placeholder="Nama Lengkap *" required="required" data-validation-required-message="Please enter your name.">
+                  <p class="help-block text-danger"></p>
+                </div>
+                <div class="form-group">
+                  <input class="form-control" id="email" type="email" placeholder="Alamat Email *" required="required" data-validation-required-message="Please enter your email address.">
+                  <p class="help-block text-danger"></p>
+                </div>
+                <div class="form-group">
+                  <input class="form-control" id="phone" type="tel" placeholder="No Handphone *" required="required" data-validation-required-message="Please enter your phone number.">
+                  <p class="help-block text-danger"></p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <textarea class="form-control" id="message" placeholder="Pesan Anda *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                  <p class="help-block text-danger"></p>
+                </div>
+              </div>
+              <div class="clearfix"></div>
+              <div class="col-lg-12 text-center">
+                <div id="success"></div>
+                <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section> -->
+
+  <!-- Footer -->
+	<section id="footer">
+		<div class="container">
+			<div class="row text-center text-xs-center text-sm-left text-md-left">
+				<!-- <div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Tentang Kami</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href="#page-top"><i class="fa fa-angle-double-right"></i>Home</a></li>
+						<li><a href="#about"><i class="fa fa-angle-double-right"></i>About</a></li>
+						<li><a href="#services"><i class="fa fa-angle-double-right"></i>Services</a></li>
+						<li><a href="#portfolio"><i class="fa fa-angle-double-right"></i>Gallery</a></li>
+						<li><a href="#contact"><i class="fa fa-angle-double-right"></i>Contact</a></li>
+					</ul>
+				</div> -->
+				<!-- <div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Cooming Soon</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Cooming Soon</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Cooming Soon</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Cooming Soon</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Cooming Soon</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Cooming Soon</a></li>
+					</ul>
+				</div> -->
+				<!-- <div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Cooming Soon</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Cooming Soon</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Cooming Soon</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Cooming Soon</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Cooming Soon</a></li>
+					</ul>
+				</div> -->
+			</div>
+			<div class="row">
+				<!-- <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+					<ul class="list-unstyled list-inline social text-center">
+						<li class="list-inline-item"><a href="https://www.facebook.com/positiverepublik/"><i class="fab fa-facebook-square"></i></a></li>
+						<li class="list-inline-item"><a href="https://www.instagram.com/positiverepublik/"><i class="fab fa-instagram"></i></a></li>
+						<li class="list-inline-item"><a href="https://www.youtube.com/channel/UCWPndxrGByoDZnsvvUxaIAA"><i class="fab fa-youtube"></i></a></li>
+					</ul>
+				</div> -->
+				</hr>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+					<p class="h6">Copyright  &copy 2019 - Software Engineering Club </p>
+				</div>
+				</hr>
+			</div>
+		</div>
+</section>
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Scroll to Top Button-->
+  <a class="wa-button rounded" href="https://wa.me/6289630080545?text=Assalamualaikum%20kak%20Isma" data-toggle="tooltip" data-placement="right" title="Whatsapp Chat">
+    <i class="fab fa-whatsapp fa-lg"></i>
+  </a>
+
+  <!-- Modal Daftar -->
+  <div class="modal fade" id="daftar" tabindex="-1" role="dialog" aria-labelledby="daftarLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="daftarLabel">Daftar Magang</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <h3>Cooming Soon...</h3>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/creative.js"></script>
+  <script type="text/javascript">
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  });
+  </script>
+
+</body>
+
+</html>
